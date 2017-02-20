16b4dfbec7d5277f2eacb12d1e46be1b9d58de30
Timothy Hurt <timothyjhurt@gmail.com>
2017-02-20 11:39:29 -0800

    making it work for all files

--- exercise_1/loading_and_modelling/load_data_lake.sh
+++ exercise_1/loading_and_modelling/load_data_lake.sh
@@ -1,19 +1,59 @@
-#!bin/bash
-file1="Hospital General Information.csv"
-file2="Timely and Effective Care - Hospital.csv"
-file3="Readmissions and Deaths - Hospital.csv"
-file4="Measure Dates.csv"
+#!/bin/bash
 
-if [-f $hvpb_hcahps_05_28_2015.csv]; then
-	mv hvpb_hcahps_05_28_2015.csv hvpb_hcahps_11_10_2016.csv
-fi
+# save current dir
+starting_dir=$(pwd)
+
+# creating staging directories
+mkdir ~/staging
+mkdir ~/staging/exercise_1
+
+# change to staging
+cd ~/staging/exercise_1
+
+# set URL
+THE_URL="https://data.medicare.gov/views/bg9k-emty/files/6c902f45-e28b-42f5-9f96-ae9d1e583472?content_type=application%2Fzip%3B%20charset%3Dbinary&filename=Hospital_Revised_Flatfiles.zip"
+
+# get zip file
+wget "$THE_URL" -O medicare_data.zip
+
+# unzip file
+unzip medicare_data.zip
+
+# remove first line of files and rename
+file1="Hospital General Information.csv"
+tail -n +2 "$file1" >hospitals.csv
+
+file2="Timely and Effective Care - Hospital.csv"
+tail -n +2 "$file2" >effective_care.csv
+
+file3="Readmissions and Deaths - Hospital.csv"
+tail -n +2 "$file3" >readmissions.csv
+
+file4="Measure Dates.csv"
+tail -n +2 "$file4" >measures.csv
 
 file5="hvbp_hcahps_11_10_2016.csv"
-
-tail -n +2 "$file1" >hospitals.csv
-tail -n +2 "$file2" >effective_care.csv
-tail -n +2 "$file3" >readmissions.csv
-tail -n +2 "$file4" >measures.csv
 tail -n +2 "$file5" >survey_responses.csv
 
 
+# create hdfs dir and move file into it
+hdfs dfs ~mkdir /user/w205/hospital_compare/hospitals
+hdfs dfs -put $hospitals.csv /user/w205/hospital_compare/hospitals
+
+hdfs dfs ~mkdir /user/w205/hospital_compare/effective_care
+hdfs dfs -put $effective_care.csv /user/w205/hospital_compare/effective_care
+
+hdfs dfs ~mkdir /user/w205/hospital_compare/readmissions
+hdfs dfs -put $readmissions.csv /user/w205/hospital_compare/readmissions
+
+hdfs dfs ~mkdir /user/w205/hospital_compare/measures
+hdfs dfs -put $measures.csv /user/w205/hospital_compare/measures
+
+hdfs dfs ~mkdir /user/w205/hospital_compare/survey_responses
+hdfs dfs -put $survey_responses.csv /user/w205/hospital_compare/survey_responses
+
+#change directory back to the original
+cd $starting_dir
+
+#clean exit
+exit
