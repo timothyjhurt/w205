578695008c29e9df5f0ef0f0da06c8862950bdeb
w205 <w205@ip-172-31-10-75.ec2.internal>
2017-02-20 20:36:29 +0000

    Fixed issues in my bash script

--- exercise_1/loading_and_modelling/load_data_lake.sh
+++ exercise_1/loading_and_modelling/load_data_lake.sh
@@ -21,36 +21,38 @@ unzip medicare_data.zip
 
 # remove first line of files and rename
 file1="Hospital General Information.csv"
-tail -n +2 "$file1" > hospitals.csv
+tail -n +2 "$file1" > "hospitals.csv"
 
 file2="Timely and Effective Care - Hospital.csv"
-tail -n +2 "$file2" > effective_care.csv
+tail -n +2 "$file2" > "effective_care.csv"
 
 file3="Readmissions and Deaths - Hospital.csv"
-tail -n +2 "$file3" > readmissions.csv
+tail -n +2 "$file3" > "readmissions.csv"
 
 file4="Measure Dates.csv"
-tail -n +2 "$file4" > measures.csv
+tail -n +2 "$file4" > "measures.csv"
 
 file5="hvbp_hcahps_11_10_2016.csv"
-tail -n +2 "$file5" > survey_responses.csv
+tail -n +2 "$file5" > "survey_responses.csv"
 
 
 # create hdfs dir and move file into it
-hdfs dfs ~mkdir /user/w205/hospital_compare/hospitals
-hdfs dfs -put $hospitals.csv /user/w205/hospital_compare/hospitals
+hdfs dfs -mkdir /user/w205/hospital_compare
 
-hdfs dfs ~mkdir /user/w205/hospital_compare/effective_care
-hdfs dfs -put $effective_care.csv /user/w205/hospital_compare/effective_care
+hdfs dfs -mkdir /user/w205/hospital_compare/hospitals
+hdfs dfs -put "hospitals.csv" /user/w205/hospital_compare/hospitals
 
-hdfs dfs ~mkdir /user/w205/hospital_compare/readmissions
-hdfs dfs -put $readmissions.csv /user/w205/hospital_compare/readmissions
+hdfs dfs -mkdir /user/w205/hospital_compare/effective_care
+hdfs dfs -put "effective_care.csv" /user/w205/hospital_compare/effective_care
 
-hdfs dfs ~mkdir /user/w205/hospital_compare/measures
-hdfs dfs -put $measures.csv /user/w205/hospital_compare/measures
+hdfs dfs -mkdir /user/w205/hospital_compare/readmissions
+hdfs dfs -put "readmissions.csv" /user/w205/hospital_compare/readmissions
 
-hdfs dfs ~mkdir /user/w205/hospital_compare/survey_responses
-hdfs dfs -put $survey_responses.csv /user/w205/hospital_compare/survey_responses
+hdfs dfs -mkdir /user/w205/hospital_compare/measures
+hdfs dfs -put "measures.csv" /user/w205/hospital_compare/measures
+
+hdfs dfs -mkdir /user/w205/hospital_compare/survey_responses
+hdfs dfs -put "survey_responses.csv" /user/w205/hospital_compare/survey_responses
 
 #change directory back to the original
 cd $starting_dir
