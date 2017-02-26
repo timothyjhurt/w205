40144e2122129e799fa242f2d775ffa961e3f710
w205 <w205@ip-172-31-10-75.ec2.internal>
2017-02-26 22:22:47 +0000

    now it works

--- exercise_1/investigations/best_hospitals.sql
+++ exercise_1/investigations/best_hospitals.sql
@@ -1,3 +1,7 @@
+create table my_eff_care_best as select id, name, avg(score) eff_score from my_eff_care where score < 100 AND score > 0 group by id, name order by avg(score) desc;
+
+create table my_read_best as select id, name, avg(score) read_score from my_readmissions where score < 100 and score > 0 group by id, name order by avg(score) desc;
+
 create table my_hospitals_best as
 select * from my_hospitals where hosp_over_rate like '5' AND
 mort_nat_comp not like 'Below%'
@@ -8,14 +12,7 @@ eff_nat_comp not like 'Below%'
 AND
 timeliness not like 'Below%'
 
-create table hosp_eff_read as 
-select my_read.name, my_read.id, my_read.score read_score, eff.score eff_score 
-from my_eff_care eff inner join my_hospitals_best hosp on eff.id=hosp.id 
-inner join my_read_best my_read on my_read.id=eff.id;
+create table hosp_eff_read as select my_read.name, my_read.id, read_score,  eff_score from my_hospitals_best hosp inner join my_eff_care_best eff on eff.id=hosp.id inner join my_read_best my_read on my_read.id=hosp.id;
 
-create table best_hospitals as 
-select id, name, eff_score, avg(read_score) 
-from hosp_eff_read 
-where eff_score<100 
-group by id, name, eff_score 
-order by eff_score desc, avg(read_score) desc;
+create table hospitals_best as
+select id, name from hosp_eff_read, eff_score*read_score order by eff_score*read_score desc;
