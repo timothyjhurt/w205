ddea03a361842f01369321398dca0e7bb3be82b2
Timothy Hurt <timothyjhurt@gmail.com>
2017-02-26 15:17:15 -0800

    forgot as in create statement

--- exercise_1/investigations/best_states.sql
+++ exercise_1/investigations/best_states.sql
@@ -3,4 +3,4 @@ create table good_hosp as select state, count(state) num_state from my_hospitals
 
 create table tot_hosp as select state, count(state) den_state from my_hospitals group by state;
 
-create table best_states select tot_hosp.state, num_state/den_state perc_good from tot_hosp inner join good_hosp on tot_hosp.state=good_hosp.state order by perc_good desc;
+create table best_states as select tot_hosp.state, num_state/den_state perc_good from tot_hosp inner join good_hosp on tot_hosp.state=good_hosp.state order by perc_good desc;
