72bb2dd9c20590b6611d4d7dd274b8ce30b94795
Timothy Hurt <timothyjhurt@gmail.com>
2017-03-04 12:03:00 -0800

    Trying to get it to run end to end

--- exercise_1/investigations/best_hospitals.sql
+++ exercise_1/investigations/best_hospitals.sql
@@ -4,11 +4,14 @@ drop table my_hospitals_best;
 drop table hosp_eff_read;
 drop table hospitals_best;
 
+create table my_eff_care_best as 
+select id, name, avg(score) eff_score from my_eff_care 
+group by id, name 
+order by eff_score desc;
 
-
-create table my_eff_care_best as select id, name, avg(score) eff_score from my_eff_care where score < 100 AND score > 0 group by id, name order by avg(score) desc;
-
-create table my_read_best as select id, name, avg(score) read_score from my_readmissions where score < 100 and score > 0 group by id, name order by avg(score) desc;
+create table my_read_best as 
+select id, name, avg(score) read_score from my_readmissions 
+group by id, name order by read_score desc;
 
 create table my_hospitals_best as
 select * from my_hospitals where hosp_over_rate like '5' AND
@@ -20,7 +23,14 @@ eff_nat_comp not like 'Below%'
 AND
 timeliness not like 'Below%';
 
-create table hosp_eff_read as select my_read.name, my_read.id, read_score,  eff_score from my_hospitals_best hosp inner join my_eff_care_best eff on eff.id=hosp.id inner join my_read_best my_read on my_read.id=hosp.id;
+create table hosp_eff_read as 
+select my_read.name, my_read.id, read_score, eff_score 
+from my_hospitals_best hosp 
+inner join my_eff_care_best eff on eff.id=hosp.id 
+inner join my_read_best my_read on my_read.id=hosp.id;
 
 create table hospitals_best as
-select id, name, eff_score*read_score score_tot from hosp_eff_read order by eff_score*read_score desc;
+select id, name, eff_score*read_score score_tot from hosp_eff_read 
+order by score_tot desc;
+
+select * from hospitals_best order by score_tot desc limit 10;
