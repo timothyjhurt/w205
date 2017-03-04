4bf2c3ab7fd6f58dfd26801d5692ffedae2d150f
Timothy Hurt <timothyjhurt@gmail.com>
2017-03-04 12:12:43 -0800

    quick save

--- exercise_1/investigations/hospitals_and_patients.sql
+++ exercise_1/investigations/hospitals_and_patients.sql
@@ -11,9 +11,10 @@ cast(h_base_score as float) score2,
 cast(h_cons_score as float) score3
 from my_surveys;
 
-create view agg_surv_scores as select id, name, state, score1*8+score2+score3*4 as score_tot from my_surv_float;
+create view agg_surv_scores as 
+select id, name, state, score1*8+score2+score3*4 as score_tot 
+from my_surv_float;
 
-
-
-
-select corr(s.score_tot, h.score_tot) from hospitals_best h inner join agg_surv_scores s on h.id=s.id;
+select corr(s.score_tot, h.score_tot) 
+from hospitals_best h 
+inner join agg_surv_scores s on h.id=s.id;
