d0d63d32869bc8469c500498315c1e54cb38a0d3
Timothy Hurt <timothyjhurt@gmail.com>
2017-03-04 14:43:59 -0800

    adding hospital rating

--- exercise_1/investigations/hospitals_and_patients.sql
+++ exercise_1/investigations/hospitals_and_patients.sql
@@ -19,13 +19,17 @@ select my_read.id,
 hosp.state,
 my_read.name, 
 read_score, 
+hosp_over_rate,
 eff_score
 from my_hospitals hosp 
 inner join my_eff_care_best eff on eff.id=hosp.id 
-inner join my_read_best my_read on my_read.id=hosp.id;
+inner join my_read_best my_read on my_read.id=hosp.id
+where hosp_over_rate >=0
+and
+hosp_over_rate <=5;
 
 create table hospitals_best as
-select id, name, state, eff_score+read_score*3.2 score_tot 
+select id, name, state, eff_score+read_score*3.2+hosp_over_rate*20 score_tot 
 from hosp_eff_read;
 
 create table my_surv_float as
