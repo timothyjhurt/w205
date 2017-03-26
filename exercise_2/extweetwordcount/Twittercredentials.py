5da8893457dd3ffa77a2d782aa2b85f9f47a543c
w205 <w205@ip-172-31-24-129.ec2.internal>
2017-03-26 20:20:00 +0000

    Fixed credentials

--- exercise_2/extweetwordcount/Twittercredentials.py
+++ exercise_2/extweetwordcount/Twittercredentials.py
@@ -1,16 +1,16 @@
 import tweepy
 
-consumer_key = "YourConsumerkey";
-#eg: consumer_key =ichtW56Qhbv300uqkL1Hhib7n";
+consumer_key = "ichtW56Qhbv300uqkL1Hhib7n";
+#eg: consumer_key ="ichtW56Qhbv300uqkL1Hhib7n";
 
 
-consumer_secret = "YourConsumerSecret";
+consumer_secret = "ByZNM826tKFHYfD5DjG7mcsOclxt69cBx82IKQ2Y7X65QT7zaj";
 #eg: consumer_secret = "ByZNM826tKFHYfD5DjG7mcsOclxt69cBx82IKQ2Y7X65QT7zaj";
 
-access_token = "YourAcessToken";
+access_token = "386660716-7LxdG9FEz4j2LzXSGcqWrOm4xAuKySqOxA5PRoFR";
 #eg: access_token = "386660716-7LxdG9FEz4j2LzXSGcqWrOm4xAuKySqOxA5PRoFR";
 
-access_token_secret = "YourTokenSecret";
+access_token_secret = "oLbxilJET2U4go4nviNvob3Q1xkfqC3EAqGpjVcLKEZ7O";
 #eg: access_token_secret = "oLbxilJET2U4go4nviNvob3Q1xkfqC3EAqGpjVcLKEZ7O";
 
 
