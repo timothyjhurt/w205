5a97d1f947e75a612ac1e6e3c4ac6e4ac89e4613
w205 <w205@ip-172-31-24-129.ec2.internal>
2017-04-02 20:48:39 +0000

    Adding python script

---
+++ exercise_2/finalresults.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import, print_function, unicode_literals
+
+from collections import Counter
+from streamparse.bolt import Bolt
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+import sys
+conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
+cur = conn.cursor()
+if len(sys.argv)>1:
+    word=sys.argv[1]
+    cur.execute("SELECT word, count from tweetwordcount WHERE word=%s",(word,))
+    records=cur.fetchall()
+    for rec in records:
+        print("Total number of occurances of the word '"+rec[0]+"':",rec[1])
+else:
+    cur.execute("SELECT * from tweetwordcount")
+    records=cur.fetchall()
+    for rec in records:
+        print(rec)
+
